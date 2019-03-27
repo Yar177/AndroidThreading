d81d8dfba4e25b4d7c35c5d81a7f8b197e3fd9a8
Yar <yarsher.dev@gmail.com>
2019-03-27 00:45:08 -0400

    init ExampleThread

--- app/src/main/java/com/yar/threading/MainActivity.java
+++ app/src/main/java/com/yar/threading/MainActivity.java
@@ -16,19 +16,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startThread(View view) {
-        for (int i=0; i<10; i++){
-            Log.d(TAG, "startThread: " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+
+        ExampleThread thread = new ExampleThread(10);
+        thread.start();
+
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+
 
     }
 
     public void stopThread(View view) {
     }
+
+
+    class ExampleThread extends Thread{
+        int seconds;
+        public ExampleThread(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i=0; i<seconds; i++){
+                Log.d(TAG, "ExampleThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    class ExampleRunnable implements Runnable{
+        int seconds;
+
+        public ExampleRunnable(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i=0; i<seconds; i++){
+                Log.d(TAG, "ExampleRunnable: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
 
 
 }
